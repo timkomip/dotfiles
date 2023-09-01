78447f66384e9230659044eca3f49b8b3e498350
Michael Timko <timkomip@gmail.com>
2023-09-01 14:16:41 -0400

    lsp settings - format on save = off

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -9,7 +9,6 @@ lsp.ensure_installed({
   'tsserver',
   'eslint',
   'lua_ls',
-  'solargraph',
   'standardrb'
 })
 
@@ -21,40 +20,30 @@ lsp.on_attach(function(client, bufnr)
     vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
   end, { buffer = bufnr })
 
+
   vim.keymap.set('n', 'gr', builtin.lsp_references, { buffer = true })
   vim.keymap.set('n', '<leader>ds', builtin.lsp_document_symbols, { buffer = true })
 end)
 
 lspconfig.lua_ls.setup(lsp.nvim_lua_ls())
-lsp.setup()
-cmp.setup({
-  mapping = {
-    -- `Enter` key to confirm completion
-    ['<CR>'] = cmp.mapping.confirm({ select = false }),
-
-    -- Ctrl+Space to trigger completion menu
-    ['<C-Space>'] = cmp.mapping.complete(),
-
-    -- Navigate between snippet placeholder
-    -- ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    -- ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+lspconfig.solargraph.setup({
+  init_options = { formatting = false },
+  settings = {
+    solargraph = {
+      diagnostics = true
+    }
   }
 })
 
--- lsp.format_on_save({
---   format_opts = {
---     async = false,
---     timeout_ms = 10000,
---   },
---   servers = {
---     ['lua_ls'] = { 'lua' },
---     ['standardrb'] = { 'ruby' },
---     -- if you have a working setup with null-ls
---     -- you can specify filetypes it can format.
---     -- ['null-ls'] = {'javascript', 'typescript'},
+lsp.setup()
+
+-- cmp.setup({
+--   mapping = {
+--     -- `Enter` key to confirm completion
+--     ['<CR>'] = cmp.mapping.confirm({ select = false }),
+
+--     -- Ctrl+Space to trigger completion menu
+--     ['<C-Space>'] = cmp.mapping.complete(),
 --   }
 -- })
 
--- (Optional) Configure lua language server for neovim
-
--- You need to setup `cmp` after lsp-zero
