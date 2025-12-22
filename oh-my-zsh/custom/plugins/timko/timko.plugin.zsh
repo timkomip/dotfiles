47300d6a9eb93d4e1054da614ec5a45c7decaeba
Michael Timko <timkomip@gmail.com>
2025-12-22 13:29:25 -0500

    refactor: reorganize dotfiles into config directory
    Move configuration files into config/ directory structure:
    - Move git config to config/git/
    - Move ideavimrc to config/
    - Move teleport.json to config/
    - Move vscode config to config/vscode/

--- oh-my-zsh/custom/plugins/timko/timko.plugin.zsh
+++
@@ -1,4 +0,0 @@
-source "$HOME/dotfiles/config/zsh/aliases.sh"
-source "$HOME/dotfiles/config/zsh/functions.sh"
-source "$HOME/dotfiles/config/zsh/fzf.sh"
-source "$HOME/dotfiles/config/zsh/plugins.zsh"
